ab2ed87f134a8bcf68c8f315b58bbbc0ef9c670b
Dmitry Vanin <dmitrylannmart@gmail.com>
2026-06-22 01:03:54 +0300

    Add modifications for request Create Materials

--- Script/Create Materials.sql
+++ Script/Create Materials.sql
@@ -3,5 +3,7 @@ CREATE TABLE Materials
 	ID INT NOT NULL,
 	MaterialName NVARCHAR(100) NOT NULL,
 	MaterialPrice MONEY NOT NULL,
+	MaterialCountry NVARCHAR(100) NOT NULL,
+	MaterialStatusImport INT NOT NULL,
 	PRIMARY KEY (ID)
 )
